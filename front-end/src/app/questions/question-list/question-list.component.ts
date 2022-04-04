8687ea9c12c8e764f36c65d651cd9309e00cce38
Vinh <narutovinh@hotmail.fr>
2022-04-04 19:36:53 +0200

    Avancement question-list.component #1

--- front-end/src/app/questions/question-list/question-list.component.ts
+++ front-end/src/app/questions/question-list/question-list.component.ts
@@ -1,26 +1,119 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Quiz} from 'src/models/quiz.model';
 import {QuizService} from 'src/services/quiz.service';
 import {Question} from 'src/models/question.model';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
     selector: 'app-question-list',
     templateUrl: './question-list.component.html',
     styleUrls: ['./question-list.component.scss']
 })
+
 export class QuestionListComponent implements OnInit {
+
+    public questionList: FormGroup;
 
     @Input()
     quiz: Quiz;
 
-    constructor(private quizService: QuizService) {
+    @Output()
+    index : EventEmitter<number> = new EventEmitter<number>();
+
+    constructor(public formBuilder: FormBuilder, private quizService: QuizService) {
+        this.initializeQuestionForm();
     }
 
     ngOnInit(): void {
+
+    }
+
+    private initializeQuestionForm(): void {
+        this.questionList = this.formBuilder.group({
+            label: ['Nouveau', Validators.required],
+            answers: this.formBuilder.array([])
+        });
+    }
+
+    get answers(): FormArray {
+        return this.questionList.get('answers') as FormArray;
+    }
+
+    private createAnswer(): FormGroup {
+        return this.formBuilder.group({
+            value: '',
+            isCorrect: false,
+        });
+    }
+
+    addAnswer(): void {
+        let i = 0;
+        for(i;i<4;i++)
+            this.answers.push(this.createAnswer());
+    }
+
+    addQuestion(): void {
+        if (this.questionList.valid) {
+            this.addAnswer();
+            const question = this.questionList.getRawValue() as Question;
+            this.quizService.addQuestion(this.quiz, question);
+            this.initializeQuestionForm();
+            this.index.emit(this.quiz.nbquestions-1);
+        }
+    }
+    indexQuestion(question): void {
+        this.index.emit(question.id);
     }
 
     deleteQuestion(question: Question): void {
         this.quizService.deleteQuestion(this.quiz, question);
     }
 
+
+
+
 }
+
+/*
+public questionForm: FormGroup;
+
+    constructor(public formBuilder: FormBuilder, private quizService: QuizService) {
+        // Form creation
+        this.initializeQuestionForm();
+    }
+
+    private initializeQuestionForm(): void {
+        this.questionForm = this.formBuilder.group({
+            label: ['', Validators.required],
+            answers: this.formBuilder.array([])
+        });
+    }
+
+    ngOnInit(): void {
+    }
+
+    get answers(): FormArray {
+        return this.questionForm.get('answers') as FormArray;
+    }
+
+    private createAnswer(): FormGroup {
+        return this.formBuilder.group({
+            value: '',
+            isCorrect: false,
+        });
+    }
+
+    addAnswer(): void {
+        this.answers.push(this.createAnswer());
+    }
+
+    addQuestion(): void {
+        if (this.questionForm.valid) {
+            const question = this.questionForm.getRawValue() as Question;
+            this.quizService.addQuestion(this.quiz, question);
+            this.initializeQuestionForm();
+        }
+    }
+ */
+
