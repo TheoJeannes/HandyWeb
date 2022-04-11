eccad0c4716151fd716aecc70dfb5bd6f72d667d
theo.jeannes <theo.jeannes@etu.unice.fr>
2022-04-11 14:50:38 +0200

    Update Model

--- front-end/src/mocks/quiz-list.mock.ts
+++ front-end/src/mocks/quiz-list.mock.ts
@@ -3,14 +3,21 @@ import { Question } from '../models/question.model';
 import {Theme} from "../models/theme.model";
 
 export const QUESTION_ACTOR: Question = {
-    id: '1',
+    id: 1,
+    quizId : 1,
     label: 'Jean Gabin a joué dans...',
     answers: [
         {
+            id: 1,
+            questionId : 1,
+            quizId : 1,
             value: 'Les tuches II',
             isCorrect: false,
         },
         {
+            id: 2,
+            questionId : 1,
+            quizId : 1,
             value: 'La grande illusion',
             isCorrect: true,
         }
@@ -19,13 +26,13 @@ export const QUESTION_ACTOR: Question = {
 
 export const QUIZ_LIST: Quiz[] = [
     {
-        id: '1',
+        id: 1,
         name: 'Les Acteurs', // What's happening if I change this value..?
         theme: 'Actor',
         questions: [QUESTION_ACTOR[0]],
     },
     {
-        id: '2',
+        id: 2,
         name: 'Les technos WEB',
         theme: 'Technos',
         questions: [],
