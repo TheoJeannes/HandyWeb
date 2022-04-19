a3c75ff140c858b5b1d840b4c97cb210af6b1129
AmbreCorreia <74357454+AmbreCorreia@users.noreply.github.com>
2022-04-19 10:16:36 +0200

    edit theme css #10

--- front-end/src/app/accueil/accueil.component.ts
+++ front-end/src/app/accueil/accueil.component.ts
@@ -11,13 +11,18 @@ export class AccueilComponent implements OnInit {
     private config: Config;
 
     constructor(private userService: UserService) {
-        this.userService.configSelected$.subscribe((config) => this.config = config);
+        this.userService.configSelected$.subscribe((config) => {
+            this.config = config;
+        });
     }
 
     ngOnInit(): void {
+        //while (this.config === undefined){}
         document.documentElement.style.setProperty('--button-color', '#1e98d7');
         document.documentElement.style.setProperty('--button-hover-color', '#166791');
         document.documentElement.style.setProperty('--button-font-color', '#FFFFFF');
+        console.log("config");
+        console.log(this.config);
         document.documentElement.style.setProperty('--button-font-size', this.config.size.toString()+'px');
     }
 }
