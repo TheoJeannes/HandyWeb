bceb148dcd3d44f4abbf9117c0ab5f9eec6aa2e1
JeannesTheo <theo.jeannes@etu.unice.fr>
2022-05-24 10:30:51 +0200

    Finitions

--- front-end/src/app/accueil/accueil.component.ts
+++ front-end/src/app/accueil/accueil.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
+import {QuizService} from '../../services/quiz.service';
+import {Quiz} from '../../models/quiz.model';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-accueil',
@@ -10,8 +13,12 @@ export class AccueilComponent implements OnInit{
 
     public admin = true;
     public offset = false;
+    public quizList: Quiz[];
 
-    constructor(public userService: UserService) {
+    constructor(public route : Router,public userService: UserService, public quizS : QuizService) {
+        this.quizS.quizzes$.subscribe((quizzes: Quiz[]) => {
+            this.quizList = quizzes;
+        });
     }
 
     ngOnInit(): void {
@@ -20,5 +27,11 @@ export class AccueilComponent implements OnInit{
             this.offset = this.userService.isOffset()
             console.log(this.admin)
         });
+    }
+
+    randomQuiz() {
+        this.quizS.retrieveQuizzes();
+        let id = this.quizList.map(e => e.id)[0];
+        this.route.navigate(["/play-quiz/"+id])
     }
 }
